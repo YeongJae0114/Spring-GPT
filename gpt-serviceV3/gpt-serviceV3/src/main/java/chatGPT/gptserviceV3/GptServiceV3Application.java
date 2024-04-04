108673ccec14151eaa8806f5d5110133c89925a1
admin <yjl8628@naver.com>
2024-04-04 09:27:38 +0900

    version3

---
+++ gpt-serviceV3/gpt-serviceV3/src/main/java/chatGPT/gptserviceV3/GptServiceV3Application.java
@@ -0,0 +1,13 @@
+package chatGPT.gptserviceV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptServiceV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptServiceV3Application.class, args);
+	}
+
+}
