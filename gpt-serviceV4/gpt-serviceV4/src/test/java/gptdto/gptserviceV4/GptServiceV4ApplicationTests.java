30efe3e4da4ba3b5d5cba3a4cc47ea81ec1d0293
admin <yjl8628@naver.com>
2024-04-07 18:57:05 +0900

    test

---
+++ gpt-serviceV4/gpt-serviceV4/src/test/java/gptdto/gptserviceV4/GptServiceV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package gptdto.gptserviceV4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptServiceV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
