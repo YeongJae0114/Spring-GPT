9e19d13c2f1f538a7d2d424c9524a4feb2c4839f
admin <yjl8628@naver.com>
2024-04-07 19:03:13 +0900

    SpringBootApplication

---
+++ gpt-serviceV4/gpt-serviceV4/src/main/java/gptdto/gptserviceV4/GptServiceV4Application.java
@@ -0,0 +1,13 @@
+package gptdto.gptserviceV4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptServiceV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptServiceV4Application.class, args);
+	}
+
+}
